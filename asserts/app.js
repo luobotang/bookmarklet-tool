a360e6d55ed9babd023d9a576b45b3fc40e2c00a
luobotang <tangkangxing@sina.com>
2016-09-26 10:25:54 +0800

    format code

--- asserts/app.js
+++ asserts/app.js
@@ -4,56 +4,56 @@ var app = app || {}
 app.util = app.util || {}
 app.util.escapeHtml = (function () {
 
-	// escape-html v1.0.3 from NPM
+  // escape-html v1.0.3 from NPM
 
-	var matchHtmlRegExp = /["'&<>]/;
+  var matchHtmlRegExp = /["'&<>]/;
 
-	return function escapeHtml(string) {
-	  var str = '' + string;
-	  var match = matchHtmlRegExp.exec(str);
+  return function escapeHtml(string) {
+    var str = '' + string;
+    var match = matchHtmlRegExp.exec(str);
 
-	  if (!match) {
-	    return str;
-	  }
+    if (!match) {
+      return str;
+    }
 
-	  var escape;
-	  var html = '';
-	  var index = 0;
-	  var lastIndex = 0;
+    var escape;
+    var html = '';
+    var index = 0;
+    var lastIndex = 0;
 
-	  for (index = match.index; index < str.length; index++) {
-	    switch (str.charCodeAt(index)) {
-	      case 34: // "
-	        escape = '&quot;';
-	        break;
-	      case 38: // &
-	        escape = '&amp;';
-	        break;
-	      case 39: // '
-	        escape = '&#39;';
-	        break;
-	      case 60: // <
-	        escape = '&lt;';
-	        break;
-	      case 62: // >
-	        escape = '&gt;';
-	        break;
-	      default:
-	        continue;
-	    }
+    for (index = match.index; index < str.length; index++) {
+      switch (str.charCodeAt(index)) {
+        case 34: // "
+          escape = '&quot;';
+          break;
+        case 38: // &
+          escape = '&amp;';
+          break;
+        case 39: // '
+          escape = '&#39;';
+          break;
+        case 60: // <
+          escape = '&lt;';
+          break;
+        case 62: // >
+          escape = '&gt;';
+          break;
+        default:
+          continue;
+      }
 
-	    if (lastIndex !== index) {
-	      html += str.substring(lastIndex, index);
-	    }
+      if (lastIndex !== index) {
+        html += str.substring(lastIndex, index);
+      }
 
-	    lastIndex = index + 1;
-	    html += escape;
-	  }
+      lastIndex = index + 1;
+      html += escape;
+    }
 
-	  return lastIndex !== index
-	    ? html + str.substring(lastIndex, index)
-	    : html;
-	}
+    return lastIndex !== index
+      ? html + str.substring(lastIndex, index)
+      : html;
+  }
 })()
 
 /*
@@ -63,13 +63,13 @@ app.util.escapeHtml = (function () {
  * - http://lisperator.net/uglifyjs/compress
  */
 app.util.uglifyCode = function (code) {
-	var ast = UglifyJS.parse(code)
-	ast.figure_out_scope()
-	ast.compute_char_frequency()
+  var ast = UglifyJS.parse(code)
+  ast.figure_out_scope()
+  ast.compute_char_frequency()
   ast.mangle_names()
-	compressor = UglifyJS.Compressor()
-	ast = ast.transform(compressor)
-	return ast.print_to_string()
+  compressor = UglifyJS.Compressor()
+  ast = ast.transform(compressor)
+  return ast.print_to_string()
 }
 
 app.util.beforeParse = function (code) {
@@ -89,58 +89,58 @@ app.util.afterParse = function (code) {
 }
 
 app.init = (function () {
-	var escapeHtml = app.util.escapeHtml
-	var beforeParse = app.util.beforeParse
-	var uglifyCode = app.util.uglifyCode
-	var afterParse = app.util.afterParse
+  var escapeHtml = app.util.escapeHtml
+  var beforeParse = app.util.beforeParse
+  var uglifyCode = app.util.uglifyCode
+  var afterParse = app.util.afterParse
 
-	var $code
-	var $btn
-	var $output
+  var $code
+  var $btn
+  var $output
 
-	function init() {
-		$code = document.getElementById('code')
-		$btn = document.getElementById('generate')
-		$output = document.getElementById('output')
+  function init() {
+    $code = document.getElementById('code')
+    $btn = document.getElementById('generate')
+    $output = document.getElementById('output')
 
-		$btn.onclick = function () {
-			var code = $code.value
-			var codeHref = makeCodeHref(code)
-			var $result = makeResultElement(codeHref)
-			appendResultToOutputBox($result)
-		}
-	}
+    $btn.onclick = function () {
+      var code = $code.value
+      var codeHref = makeCodeHref(code)
+      var $result = makeResultElement(codeHref)
+      appendResultToOutputBox($result)
+    }
+  }
 
-	function makeCodeHref(code) {
-  	code = beforeParse(code)
-  	code = uglifyCode('void(function(){' + code + '}())')
-  	code = afterParse(code)
-		return 'javascript:' + escapeHtml(code)
-	}
+  function makeCodeHref(code) {
+    code = beforeParse(code)
+    code = uglifyCode('void(function(){' + code + '}())')
+    code = afterParse(code)
+    return 'javascript:' + escapeHtml(code)
+  }
 
-	function makeResultElement(codeHref) {
-		var $result = document.createElement('div')
-		$result.className = 'result-item'
-		$result.innerHTML = (
-			'<p><a href="' + codeHref + '">Bookmarklet</a></p>' +
-			'<pre><code>' + codeHref + '</code></pre>'
-		)
-		return $result
-	}
+  function makeResultElement(codeHref) {
+    var $result = document.createElement('div')
+    $result.className = 'result-item'
+    $result.innerHTML = (
+      '<p><a href="' + codeHref + '">Bookmarklet</a></p>' +
+      '<pre><code>' + codeHref + '</code></pre>'
+    )
+    return $result
+  }
 
-	function appendResultToOutputBox($result) {
-		var $firstChild = $output.children && $output.children[0]
-		if ($firstChild) {
-			$output.insertBefore($result, $firstChild)
-		} else {
-			$output.appendChild($result)
-		}
-	}
+  function appendResultToOutputBox($result) {
+    var $firstChild = $output.children && $output.children[0]
+    if ($firstChild) {
+      $output.insertBefore($result, $firstChild)
+    } else {
+      $output.appendChild($result)
+    }
+  }
 
-	return init
+  return init
 })()
 
 
 window.onload = function () {
-	app.init()
+  app.init()
 }
